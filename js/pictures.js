770dcaac0e7960d095a6279a52cf60d84ccc9fa0
Anastasiya Alekseeva <anastasiyaalexxx@gmail.com>
2025-12-21 00:20:27 +0300

    Добавляет отрисовку миниатюр фото

---
+++ js/pictures.js
@@ -0,0 +1,23 @@
+const templatePicture = document.querySelector('#picture').content.querySelector('.picture');
+const containerPictures = document.querySelector('.pictures');
+
+const renderPictures = (photos) => {
+  const fragment = document.createDocumentFragment();
+
+  photos.forEach((photo) => {
+    const picture = templatePicture.cloneNode(true);
+    const image = picture.querySelector('.picture__img');
+
+    image.src = photo.url;
+    image.alt = photo.description;
+
+    picture.querySelector('.picture__comments').textContent = photo.comments.length;
+    picture.querySelector('.picture__likes').textContent = photo.likes;
+
+    fragment.appendChild(picture);
+  });
+
+  containerPictures.appendChild(fragment);
+};
+
+export { renderPictures };
